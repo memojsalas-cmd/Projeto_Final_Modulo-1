a178966f1cc8f94eecc00f8e8fec97ddbea7fd19
memojsalas-cmd <memojsalas@gmail.com>
2026-07-17 11:34:42 -0300

    teste

--- src/modeling/train.py
+++ src/modeling/train.py
@@ -12,11 +12,10 @@ from src.config import MODEL_DIR, MODEL_FILE, METRICS_FILE
 
 
 def train_model(X_train: pd.DataFrame, y_train: pd.Series) -> LinearRegression:
-    
-    """Treina uma Regressão Linear simples nos dados de treino."""
-    model = LinearRegression()
-    model.fit(X_train, y_train)
-    return model
+  """Treina uma Regressão Linear simples nos dados de treino."""
+  model = LinearRegression()
+  model.fit(X_train, y_train)
+  return model
 
 
 def train_knn_model(
@@ -24,6 +23,7 @@ def train_knn_model(
 ) -> KNeighborsRegressor:
     
     """Treina um KNN Regressor para comparação com a Regressão Linear."""
+    
     if y_train is None:
         if not isinstance(X_train, tuple) or len(X_train) != 2:
             raise TypeError("Informe X_train e y_train para treinar o modelo KNN.")
@@ -35,6 +35,7 @@ def train_knn_model(
 
 
 def evaluate_model(model: LinearRegression, X, y_true) -> dict:
+    
     """Calcula MAE, MSE, RMSE e R2 das previsões do modelo."""
     y_pred = model.predict(X)
     mse = mean_squared_error(y_true, y_pred)
@@ -46,25 +47,28 @@ def evaluate_model(model: LinearRegression, X, y_true) -> dict:
     }
 
 def evaluate_model2(model: KNeighborsRegressor, X, y_true) -> dict:
-    """Calcula MAE, MSE, RMSE e R2 das previsões do modelo."""
-    y_pred = model.predict(X)
-    mse = mean_squared_error(y_true, y_pred)
-    return {
+   """Calcula MAE, MSE, RMSE e R2 das previsões do modelo."""
+        
+   y_pred = model.predict(X)
+   mse = mean_squared_error(y_true, y_pred)
+   return {
         "MAE": mean_absolute_error(y_true, y_pred),
         "MSE": mse,
         "RMSE": np.sqrt(mse),
         "R2": r2_score(y_true, y_pred),
     }   
 
-def save_model(model: LinearRegression) -> None:
-    """Salva o modelo treinado em models/v1/."""
+def save_model(model, model_name: str) -> None:
+    """ Salva o modelo campeão utilizando seu nome. """
+    
     MODEL_DIR.mkdir(parents=True, exist_ok=True)
+    MODEL_FILE = MODEL_DIR / f"{model_name}.pkl"
     joblib.dump(model, MODEL_FILE)
-    print(f"Modelo salvo em: {MODEL_FILE}")
-
+    print(f"Modelo campeão ({model_name}) salvo em: {MODEL_FILE}")
 
 def save_metrics(metrics: dict) -> None:
     """Salva o dicionário de metadados/métricas em models/v1/."""
+   
     MODEL_DIR.mkdir(parents=True, exist_ok=True)
     with open(METRICS_FILE, "w", encoding="utf-8") as f:
         json.dump(metrics, f, indent=2, ensure_ascii=False)
